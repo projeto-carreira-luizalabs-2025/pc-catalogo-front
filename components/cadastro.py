27119ffcc0949df4d576537f5a88c5f628b59960
GitBella <au.isabella.r.c@hotmail.com>
2025-07-09 22:37:09 -0300

    chore: adicionado loading ao cadatsrar produto

--- components/cadastro.py
+++ components/cadastro.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import cadastrar_produto
 
 def tela_cadastro():
-    st.markdown("<h2 style='text-align: center;'>Cadastrar Produto</h2>", unsafe_allow_html=True)
+    st.title("📦 Cadastrar Produto")
 
     sku = st.text_input("SKU do Produto")
     nome = st.text_input("Nome do Produto")
@@ -11,7 +11,10 @@ def tela_cadastro():
         if not sku or not nome:
             st.warning("Preencha todos os campos.")
         else:
-            if cadastrar_produto(sku, nome):
+            with st.spinner("Cadastrando produto..."):
+                sucesso = cadastrar_produto(sku, nome)
+
+            if sucesso:
                 st.success("Produto cadastrado com sucesso!")
             else:
                 st.error("Erro ao cadastrar produto.")
